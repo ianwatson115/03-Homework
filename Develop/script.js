f5205e61fc295432bf34b8e5774b2ed4bdb59549
ian <ianwatson115@gmail.com>
2019-12-03 17:36:06 -0500

    created a generate function with a framework of a for loop that it will cycle through to generate the password

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,27 @@
+var generate = document.getElementById("#generate");
+var copy = document.getElementById("#copy")
+var askLength = prompt("How long do you want your password to be?(enter a number from 8 to 128)"); 
+var length = 0;
 
-var length = prompt("How long do you want your password to be?(enter a number from 8 to 128)"); 
-var chars = prompt("Enter the special characters you want to include within your password");
 
-if (length.parseInt < 8 || length.parseInt > 128){
+if (askLength.parseInt < 8 || askLength.parseInt > 128){
     alert("You have entered an invalid number, please reload and try again!");
 } else {
-    
+    var askChars = prompt("Enter the special characters you want to include within your password");
 }
-length.parseInt("")
-function generate(){
+function generate() {
     //set the password length
+    var passLength = askLength.parseInt();
+    //characters in password
+    var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    var value = ""
+    var special = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "^", "_", "`", "{", "}", "~", "\\"];
+    
+    
+    for(var i = 0, n = charset.length; i < passLength; i++){
+
+    }
+
 }
+
+generate.addEventListener("click", generate);
