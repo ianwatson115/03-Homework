34e79b8314648b60ad07ebb3e0bd55883fcb42e2
ian <ianwatson115@gmail.com>
2019-12-03 12:42:09 -0500

    added more css and created a box for my content. Started some prompts for the user to click through.

---
+++ Develop/script.js
@@ -0,0 +1,13 @@
+
+var length = prompt("How long do you want your password to be?(enter a number from 8 to 128)"); 
+var chars = prompt("Enter the special characters you want to include within your password");
+
+if (length.parseInt < 8 || length.parseInt > 128){
+    alert("You have entered an invalid number, please reload and try again!");
+} else {
+    
+}
+length.parseInt("")
+function generate(){
+    //set the password length
+}
