dddfa823fb9b53c4859cc51703994ff3416749aa
ian <ianwatson115@gmail.com>
2019-12-03 19:31:50 -0500

    made an inner for loop to loop through the special characters

--- Develop/script.js
+++ Develop/script.js
@@ -8,20 +8,29 @@ if (askLength.parseInt < 8 || askLength.parseInt > 128){
     alert("You have entered an invalid number, please reload and try again!");
 } else {
     var askChars = prompt("Enter the special characters you want to include within your password");
+    if (askChars.length > askLength.parseInt){
+        alert("You have entered an invalid amount of characters, please reload and try again!");
+    } 
 }
+
 function generate() {
     //set the password length
     var passLength = askLength.parseInt();
     //characters in password
     var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    var value = ""
+    var password = "";
     var special = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "^", "_", "`", "{", "}", "~", "\\"];
-    
+    var specChar = "";
     
     for(var i = 0, n = charset.length; i < passLength; i++){
-
+        for(var j = 0, m = special.length; j < askChars.length; j++){
+            charset += special.charAt(Math.floor(Math.random()*m));
+        }
+        password += charset.charAt(Math.floor(Math.random()*n));
     }
-
+    return password;
 }
 
-generate.addEventListener("click", generate);
+
+
+generate.addEventListener("click", generate());
