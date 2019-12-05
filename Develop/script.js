ae98a4c64dfaa2e1c6ee27a0c84569b6d6a582e8
ian <ianwatson115@gmail.com>
2019-12-05 13:28:37 -0500

    completely revamped my html and adapted it to my javascript. Added checkboxes as prompts would be annoying for the user to cycle through

--- Develop/script.js
+++ Develop/script.js
@@ -1,54 +1,54 @@
-var generate = document.getElementById("generate");
-console.log(generate);
+
 var copy = document.getElementById("copy")
-var length = 0;
+var length = document.getElementById("length");
+//all the character I will need 
 var special = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "^", "_", "`", "{", "}", "~", "\\"];
 var smallCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var bigCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-console.log(special);
-console.log(smallCase);
-console.log(bigCase);
-console.log(numbers);
 
-
-
+console.log(length);
 
 function generatePassword() {
-    var askLength = prompt("How long do you want your password to be?(enter a number from 8 to 128)"); 
-    if (parseInt(askLength) >= 8 && parseInt(askLength) <= 128) {
-        var askChars = prompt("Select what you would like to include in your password: ");
-        if (parseInt(askChars) > 4 || parseInt(askChars) < 1 ){
-            alert("You have entered an invalid amount of characters, please reload and try again!");
-        } else if (parseInt(askChars) >= 1 && parseInt(askChars) <= 4) {
-            
-        
-    }
-        console.log(askLength);
-        console.log(askChars);
-        
+    // var askLength = prompt("How long do you want your password to be?(enter a number from 8 to 128)"); 
+    // if (parseInt(askLength) >= 8 && parseInt(askLength) <= 128) {
+    //     var askChars = prompt("Select what you would like to include in your password: ");
+    //     if (parseInt(askChars) > 4 || parseInt(askChars) < 1 ){
+    //         alert("You have entered an invalid amount of characters, please reload and try again!");
+    //     } else if (parseInt(askChars) >= 1 && parseInt(askChars) <= 4) {
+    // }
+    //     console.log(askLength);
+    //     console.log(askChars);
+    // } else {
+    //     alert("You have entered an invalid number, please reload and try again!");
+    // }
+    if (parseInt(length) >= 8 && parseInt(length) <= 128) {
+        console.log(length);
+        password();
     } else {
-        alert("You have entered an invalid number, please reload and try again!");
+            // alert("You have entered an invalid number, please reload and try again!");
     }
+
     
-    //
     
     
     //set the password length
-    var passLength = parseInt(askLength);
-    console.log(passLength);
-    //characters in password
-    var password = "";
-    var specChar = "";
-    
-    for(var i = 0, n = charset.length; i < passLength; i++){
-        for(var j = 0, m = special.length; j < askChars.length; j++){
-            specChar += special*(Math.floor(Math.random()*m));
+    function password(){
+        var passLength = parseInt(length);
+        console.log(passLength);
+        //characters in password
+        var password = "";
+        var specChar = "";
+        
+        for(var i = 0, n = charset.length; i < passLength; i++){
+            for(var j = 0, m = special.length; j < askChars.length; j++){
+                specChar += special*(Math.floor(Math.random()*m));
+            }
+            password += charset.charAt(Math.floor(Math.random()*n)) + specChar;
+            console.log(password);
         }
-        password += charset.charAt(Math.floor(Math.random()*n)) + specChar;
-        console.log(password);
+        return document.getElementById("password").textContent = password;
     }
-    return document.getElementById("password").textContent = password;
 
 }
 
